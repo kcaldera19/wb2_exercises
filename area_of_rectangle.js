20ce71e3d55f8c1a04453b63ae4fa9834b5e73b6
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 11:30:12 -0500

    area of rectangle calculation

---
+++ area_of_rectangle.js
@@ -0,0 +1,10 @@
+// creating variables
+let length = 70;
+let width = 35;
+
+// multiply the variables above to give the area
+
+let areaOfRectangle = length * width;
+
+// output result for area of a rectangle
+console.log("The area of the rectangle is " + areaOfRectangle)
