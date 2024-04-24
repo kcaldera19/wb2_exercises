5826e60dfa8db0273b9b34b5be0fe58f6121cca5
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 14:07:30 -0500

    rule 72

---
+++ rule_of_72.js
@@ -0,0 +1,14 @@
+// variables created as inputs for calculation
+let interestRate = 3.5;
+let savings = 300.00;
+
+let year = 72 / interestRate;
+
+// output of how many years to double 
+ console.log("At a 3.5% "+ interestRate+ "your saving account will be worth $600.00 in "+ year+" years" )
+
+// fixed 2 digit decimal point
+console.log(year.toFixed(2))
+
+// fixed 1 digit deciaml point
+console.log(year.toFixed(1))
