f24e4a22b7007da9c45a47ab9634bb4880b445c8
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 10:57:38 -0500

    tips calculation

---
+++ tips.js
@@ -0,0 +1,10 @@
+// variables created as inputs for calculation
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+// do our calcluations for the variables above
+let totalDue = foodCost + tax + tip;
+
+// output the result to the console
+console.log("The total due is " + totalDue);
