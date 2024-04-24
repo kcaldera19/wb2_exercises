8e0d25e198f7e9908e50c36b0424cec83b62cf56
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 11:43:37 -0500

    tip calculation for 15 percent

---
+++ tips_amount.js
@@ -0,0 +1,7 @@
+// Variables created
+let foodBill = 170.00;
+let tipPercentage = 0.15;
+
+// do our calcluations for the variables above
+let tip = foodBill * tipPercentage;
+console.log("The tip on a $170.00 food bill is  " + tip)
