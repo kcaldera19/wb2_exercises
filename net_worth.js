cd5e2c3c5344c59b8b423a2fae9db65a11e05e22
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 14:34:25 -0500

    fixed the - in net worth

--- net_worth.js
+++ net_worth.js
@@ -2,7 +2,7 @@
 let assets = 20000.00
 let debt = 1500.00
 // caluculation for the variables above
-let netWorth = assets + debt;
+let netWorth = assets - debt;
 
 // net worth output 
 console.log("Your net worth is " + netWorth)
