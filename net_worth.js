40984901c1b5a92be309df4d0da1241740ea4cbb
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 11:19:02 -0500

    net worth calculation

---
+++ net_worth.js
@@ -0,0 +1,8 @@
+// varibale created for the inputs calculation
+let assets = 20000.00
+let debt = 1500.00
+// caluculation for the variables above
+let netWorth = assets + debt;
+
+// net worth output 
+console.log("Your net worth is " + netWorth)
