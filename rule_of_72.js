06b58ee77ec05b3fc9d70a76e9bd7b3844ecd244
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-24 15:03:52 -0500

    added fixed decimal

--- rule_of_72.js
+++ rule_of_72.js
@@ -1,14 +1,16 @@
 // variables created as inputs for calculation
 let interestRate = 3.5;
 let savings = 300.00;
+let doublebalance = savings * 2 
 
 let year = 72 / interestRate;
+console.log("the number of years to double my moneyis " + year)
 
 // output of how many years to double 
- console.log("At a 3.5% "+ interestRate+ "your saving account will be worth $600.00 in "+ year+" years" )
+ console.log("At a 3.5% "+ interestRate+ " your saving account will be worth "+ doublebalance.toFixed(2) + " in " + year.toFixed(1)+ " years " )
 
 // fixed 2 digit decimal point
-console.log(year.toFixed(2))
+// console.log(year.toFixed(2))
 
 // fixed 1 digit deciaml point
-console.log(year.toFixed(1))
+// console.log(year.toFixed(1))
