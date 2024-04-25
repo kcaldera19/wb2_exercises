022fbe80e88139026f8697013dd920d54163948a
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-25 11:58:05 -0500

    conversion test of parseint,parsefloat,and number

---
+++ conversion_tests.js
@@ -0,0 +1,24 @@
+"use strict"
+// the variable we are working with
+let a = " 101.1 ";
+let b = "55";
+let c = "402 Stevens";
+let d = "Number 5  ";
+
+// using parseint for each variable and displaying the results
+console.log("variable a with parsseInt; "+ parseInt(a));
+console.log("varibale b with parsseInt; "+ parseInt(b));
+console.log("variable c with parsseInt; "+ parseInt(c));
+console.log("variable d with parsseInt; "+ parseInt(d));
+
+// using parseFloat for each variable and displaying the results
+console.log("variable a with parseFloat;"+ parseFloat(a));
+console.log("variable b with parseFloat;"+ parseFloat(b));
+console.log("variable c with parseFloat;"+ parseFloat(c));
+console.log("variable d with parseFloat;"+ parseFloat(d));
+
+// using parseFloat for each variable and displaying the results
+console.log("varibale a with number;"+ Number(a));
+console.log("varibale b with number;"+ Number(b));
+console.log("varibale c with number;"+ Number(c));
+console.log("varibale d with number;"+ Number(d));
