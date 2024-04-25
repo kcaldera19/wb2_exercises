afe256ac77ccad7e49dc4c6daed8f3f75731d9cd
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-25 10:05:47 -0500

    distance between the two points

---
+++ distance.js
@@ -0,0 +1,10 @@
+let x1 = 2;
+let y1 = 3;
+let x2 = 5;
+let y2 = 7;
+
+let firstPoint = x1 - x2;
+let secondPoint = y1 -y2;
+
+let distance = Math.sqrt(firstPoint* firstPoint + secondPoint*secondPoint)
+console.log(distance +" is the distance")
