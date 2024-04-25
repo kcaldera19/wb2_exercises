f994f5c2b6a35847a710636078c0f7571a4b0d7a
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-25 09:23:49 -0500

    fahrenheit to celsius

--- rule_of_72.js
+++ rule_of_72.js
@@ -4,7 +4,7 @@ let savings = 300.00;
 let doublebalance = savings * 2 
 
 let year = 72 / interestRate;
-console.log("the number of years to double my moneyis " + year)
+console.log("the number of years to double my money is " + year)
 
 // output of how many years to double 
  console.log("At a 3.5% "+ interestRate+ " your saving account will be worth "+ doublebalance.toFixed(2) + " in " + year.toFixed(1)+ " years " )
